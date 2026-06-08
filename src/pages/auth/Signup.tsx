241a7b165f4cbeaf35aaebcb8668bfee596eb3d4
kelvinobuvie <kelvinobuvie@gmail.com>
2026-06-08 10:57:53 +0100

    FEAT: FRONT END CHANGES IN SIGNUP AUTH

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -129,7 +129,9 @@ const Signup = () => {
             className="w-full h-12 rounded-full justify-center gap-3"
             disabled={loading}
             onClick={handleGoogleSignup}
-                          <svg className="h-5 w-5" viewBox="0 0 24 24">
+              
+          >
+            <svg className="h-5 w-5" viewBox="0 0 24 24">
                 <path
                   d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z"
                   fill="#4285F4"
@@ -147,7 +149,7 @@ const Signup = () => {
                   fill="#EA4335"
                 />
               </svg>
-          >
+            
             Sign up with Google
           </Button>
 
